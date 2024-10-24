19b108d46df44f99b2529d02c730f973dc17e670
Vishal270406 <vishal.shanmugam270406@gmail.com>
2024-10-24 22:05:03 +0100

    Add files via upload

---
+++ Task B_Trikommerce Project/IMS_simulation.py
@@ -0,0 +1,10 @@
+total_days =  50
+daily_data = []
+from restock import restock_inventory
+from sales import daily_sales
+
+def run_simulation(total_days, inventory_records):
+    available_items = 2000
+    for current_day in range(0, total_days):
+        available_items = restock_inventory(available_items, inventory_records, current_day)
+        available_items = daily_sales(available_items, inventory_records, current_day)
